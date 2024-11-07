5ede5e471785b3eb7b7f55a89c09a3d0846740ff
KienNguyenAI <kiennguyen281005@gmail.com>
2024-11-07 19:01:26 +0700

    Tạo thành công giao diện card còn bị lỗi avatar và reponsive

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -1,55 +1,95 @@
 <template>
-    <div class="card flex">
-        <div class="row">
-            <div class="col-4 align-items-center justify-content-center">
-                <img src="../assets/download.jpeg" alt="" class="img-card">
+    <div class="card d-flex p-0 mt-3 mb-3">
+        <a href="" class="thumbnail">
+            <div class="container">
+                <img src="../assets/download.jpeg" alt="thumbnail" class="img-card">
             </div>
-            <div class="col-8 ps-0">
-                <div class="row mb-1">
-                    <div class=" col-10 d-flex ">
-                        <span class="category">QUAN ĐIỂM - TRANH LUẬN</span>
-                        <span class="time"> 4 phút đọc</span>
-                    </div>
-                    <div class="col-2 justify-content-end">
-                        <i class="fa-regular fa-bookmark me-3" style="color: #c3c3c3;"
-                            onclick="this.classList.toggle('fa-solid')"></i>
-                        <i class="fa-solid fa-ellipsis-vertical"></i>
-                    </div>
-                </div>
-                <div class="row mb-1">
-                    <span class="title">HCTVPG #6 - Vì sao bạn sẽ muốn đuổi Shakyamuni ra khỏi nhà?</span>
-                </div>
-                <div class="row mb-4">
-                    <span class="description">Hồi đó tôi luôn có một thắc mắc kỳ lạ rằng liệu Shakyamuni sẽ ở với tôi
-                        được bao lâu. Ba ngày? Một tiếng? Hay 15 phút?...</span>
-                </div>
-                <div class="row">
-                    <div class=" col-10 d-flex pe-0  align-items-center">
-                        <div class="avatar-container">
-                            <img src="../assets/download.jpeg" alt="Avatar" class="avatar-image">
-                        </div>
-                        <span class="username">Trịnh Trần phương tuấn</span>
-                        <span class="time"> 4 phút đọc</span>
-                    </div>
-                    <div class="col-2 d-flex align-items-center">
-                        <div class=" me-3 d-flex align-items-center">
-                            <i class="fa-solid fa-caret-up fa-xl"></i>
-                            <span style="font-size: 0.75rem;" class="ms-1">19</span>
-                        </div>
 
-                        <i class="fa-solid fa-ellipsis-vertical"></i>
+        </a>
+        <div class="card-bottom">
+            <div class="main">
+                <div class="heading-contain  mb-3 d-flex justify-content-between">
+                    <div class="d-flex align-items-center">
+                        <a href="" class="text-decoration-none">
+                            <span class="category">QUAN ĐIỂM - TRANH LUẬN</span>
+                        </a>
+                        <span style="color: #c3c3c3;">&emsp;|&emsp;</span>
+                        <span class="time">4 phút đọc</span>
+                    </div>
+                    <div class="d-flex">
+                        <i class="fa-regular fa-bookmark me-3" style="color: #c3c3c3; cursor: pointer;"
+                            onclick="this.classList.toggle('fa-solid')"></i>
+                        <i class="fa-solid fa-ellipsis-vertical" style="cursor: pointer;"></i>
+                    </div>
+                </div>
+
+                <div class="content">
+                    <div class="text">
+                        <a href="" class="text-decoration-none">
+                            <h1 class="title">HCTVPG #6 - Vì sao bạn sẽ muốn đuổi Shakyamuni ra khỏi nhà?</h1>
+                        </a>
+                    </div>
+                    <div class="description">
+                        Hồi đó tôi luôn có một thắc mắc kỳ lạ rằng liệu Shakyamuni sẽ ở với tôi được bao lâu. Ba ngày?
+                        Một tiếng? Hay 15 phút?...
+                    </div>
+                </div>
+            </div>
+            <div class="post-details d-flex justify-content-center align-items-center ">
+                <a href="" class="me-3">
+                    <a-avatar :size="48">
+
+                    </a-avatar>
+                </a>
+                <div class="details">
+                    <div class="author mb-1">
+                        <a href="" class="text-decoration-none text-black fw-bold">Trịnh Trần Phương Tuấn</a>
+                    </div>
+                    <div class="d-flex justify-content-between">
+                        <div class="create-at">17 giờ trước</div>
+                        <div class="interactions d-flex">
+                            <div class="me-2">
+                                <i class="fa-regular fa-bookmark me-1" style="color: #c3c3c3; cursor: pointer;"
+                                    onclick="this.classList.toggle('fa-solid')"></i>
+                                <span style="font-size: .75rem;">5</span>
+                            </div>
+                            <div class="me-2">
+                                <i class="fa-regular fa-eye me-1"></i>
+                                <span style="font-size: .75rem;">5</span>
+                            </div>
+                            <div class="me-2">
+                                <i class="fa-regular fa-comment me-1"></i>
+                                <span style="font-size: .75rem;">5</span>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+
 </template>
 
 <style>
 .card {
     width: 100%;
-    height: 13rem;
+    display: flex;
     box-shadow: 0 0.25rem 0.375rem rgba(0, 0, 0, 0.1);
+}
+
+.thumbnail {
+    flex: 0 0 22rem;
+    width: 22rem;
+    height: 11rem;
+    overflow: hidden;
+    margin-right: 1rem;
+}
+
+.container {
+    position: relative;
+    width: 100%;
 }
 
 .img-card {
@@ -59,8 +99,15 @@
     border: 0.025rem solid #dbdbdb;
 }
 
-.category {
-    margin-right: 1rem;
+.card-bottom {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    margin-bottom: .5rem;
+}
+
+.category,
+.time {
     font-size: 0.75rem;
     color: #4d4d4d;
 }
@@ -69,44 +116,34 @@
     font-weight: bold;
 }
 
-.time {
-    font-size: 0.75rem;
-    color: #b4b4b4;
-}
-
 .title {
-
-    padding: 0.5rem;
     font-size: 1.25rem;
-    font-weight: bold;
-
+    color: black;
+    margin-bottom: .5rem;
+    max-height: 3rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
 }
 
 .description {
-    font-size: 0.875rem;
-    color: #4d4d4d;
-}
-
-.avatar-container {
-    width: 3rem;
-    height: 3rem;
+    overflow-y: auto;
+    max-height: 3rem;
+    color: #666;
+    word-wrap: break-word;
     overflow: hidden;
-    border-radius: 50%;
+    text-overflow: ellipsis;
+    margin-bottom: .5rem;
+}
+
+
+.details {
     display: flex;
-    justify-content: center;
-    align-items: center;
+    flex-grow: 1;
+    flex-direction: column;
 }
 
-.avatar-image {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-}
-
-.username {
-    padding-left: 0.5rem;
-    padding-right: 0.5rem;
-    font-size: 0.875rem;
-    font-weight: bold;
+.create-at {
+    font-size: .75rem;
+    color: #4d4d4d;
 }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -12,18 +12,13 @@ import 'ant-design-vue/dist/reset.css';
 import "bootstrap/dist/css/bootstrap-grid.min.css";
 import "bootstrap/dist/css/bootstrap-utilities.min.css";
 
-import {
-    Input,
-    Button,
-    Menu
-} from 'ant-design-vue';
+import Antd from 'ant-design-vue';
 
 
 
 const app = createApp(App);
 // app.use(createPinia());
-app.use(Input);
-app.use(Button);
-app.use(Menu);
+
+app.use(Antd);
 app.use(router);
 app.mount('#app');
